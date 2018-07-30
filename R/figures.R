5c36324f23dd1bbdfcd11f1f8f2c95868c3c5a93
Luke Johnston <lwjohnst@gmail.com>
2018-07-30 12:53:23 +0200

    moved r scripts into R folder

--- R/figures.R
+++ R/figures.R
@@ -4,20 +4,25 @@
 library(tidyverse)
 
 disease_occurrence <- tibble(
-    Participant = fct_inorder(as.character(1:10)),
-    Status = sample(c(rep("Disease", times = 4),
-                      rep("Healthy", times = 6)),
+    Participant = fct_inorder(as.character(1:20)),
+    Status = sample(c(rep("Disease", times = 8),
+                      rep("Healthy", times = 12)),
                     replace = FALSE),
-    DiseaseTime = sample(5:14, size = 10, replace = FALSE),
+    DiseaseTime = sample(5:14, size = 20, replace = TRUE),
     Time = if_else(Status == "Healthy", 16L, DiseaseTime)
 )
 
-disease_occurrence %>%
+pro_cohort_visual <- disease_occurrence %>%
     ggplot(aes(y = Participant, xmax = Time, xmin = 0, colour = Status)) +
-    geom_errorbarh(height = 0) +
+    geom_errorbarh(height = 0, size = 1) +
+    geom_point(aes(x = Time)) +
     geom_vline(xintercept = 15, linetype = "dotted") +
     geom_vline(xintercept = 0, linetype = "solid") +
-    coord_cartesian(xlim = c(-0.5, 15), ylim = c(0, 10.5), expand = FALSE) +
+    coord_cartesian(
+        xlim = c(-0.5, 15),
+        ylim = c(0, nrow(disease_occurrence) + 0.5),
+        expand = FALSE
+    ) +
     scale_x_continuous(breaks = c(0, 15), labels = c("Start", "End")) +
     labs(x = "Follow-up time", title = "Participants in prospective cohort") +
     theme(
@@ -28,7 +33,8 @@ disease_occurrence %>%
         plot.title = element_text(size = 10),
         axis.title.x = element_text(size = 10),
         legend.title = element_blank(),
-        legend.box.background = element_blank(),
-        legend.background = element_blank()
+        legend.key = element_blank()
     )
+
+ggsave("datasets/prospective-cohort-visual-example.pdf", pro_cohort_visual)
 
