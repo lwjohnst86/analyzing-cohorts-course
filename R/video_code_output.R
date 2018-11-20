6514d31cb21664e3122d87823f7a12e45514911b
Luke Johnston <lwjohnst@gmail.com>
2018-11-20 09:32:29 +0100

    ideas and code for images

--- R/video_code_output.R
+++ R/video_code_output.R
@@ -6,8 +6,10 @@ library(ggrepel)
 library(patchwork)
 library(rlang)
 library(Epi)
+library(DiagrammeR)
+library(DiagrammeRsvg)
+library(rsvg)
 data("diet")
-
 color_theme <- ghibli_palette("MononokeMedium")
 
 # Chapter 1, prospective cohort outcomes ----------------------------------
@@ -247,11 +249,115 @@ histo_density <- function(.data, x) {
     xvar <- enquo(x)
     ggplot(transformed, aes(x = !!xvar, y = stat(density))) +
         geom_histogram(colour = "black", fill = color_theme[7], size = 0.25) +
-        geom_density()
+        geom_density() +
+        theme_gray(base_size = 18)
 }
 
 plot_transform_weight <- histo_density(transformed, weight) +
     histo_density(transformed, weight_scale) + ylab("") +
     histo_density(transformed, weight_log) +
     histo_density(transformed, weight_invert) + ylab("")
-ggsave("datasets/plot-transform-weight.png", dpi = 100)
+ggsave("datasets/ch2-v3-transform-weight.png", width = 7, height = 6)
+
+
+# Chapter 3 video 2 -------------------------------------------------------
+
+grviz_to_png <- function(graph_viz, filename, width = NULL, height = NULL) {
+    graph_viz %>%
+        export_svg() %>%
+        charToRaw() %>%
+        rsvg::rsvg_png(file = filename, width = width, height = height)
+}
+
+confounding_dag <- grViz("
+digraph {
+    node [style = filled fillcolor = none]
+    {
+        rank = same
+        Exposure [fillcolor = 'LightBlue']
+        Outcome [fillcolor = 'OrangeRed']
+    }
+    Exposure -> Outcome
+    Confounder -> {Exposure Outcome}
+}")
+
+grviz_to_png(confounding_dag,
+             "datasets/ch3-v2-classic-confounder.png",
+             width = 1500)
+
+# inspired by https://blog.revolutionanalytics.com/2013/07/a-great-example-of-simpsons-paradox.html
+tribble(
+    ~time, ~median_income, ~job_type,
+    0, 6.5, 1,
+    0, 6.25, 1,
+    0, 6, 1,
+    1, 6.5, 1,
+    1, 6.75, 1,
+    1, 6.25, 1,
+    1, 6, 1,
+    1, 6.1, 1,
+    0, 5.5, 2,
+    0, 5.25, 2,
+    0, 5, 2,
+    0, 5.3, 2,
+    0, 5.4, 2,
+    1, 5, 2,
+    1, 4.75, 2,
+    1, 4.9, 2
+) %>%
+    mutate(median_income = median_income * 10) %>%
+    ggplot(aes(x = time, y = median_income)) +
+    geom_smooth(method = "lm", se = FALSE, colour = "grey40") +
+    # geom_smooth(aes(group = job_type), method = "lm", se = FALSE, colour = "grey40") +
+    geom_point(colour = color_theme[6], size = 4)
+
+
+
+# dag_example <-
+grViz("
+digraph {
+    Node1 -> Node2 [dir = backward, xlabel = 'Edge ']
+    Node1 -> Node3 [dir = forward, xlabel = '     Edge']
+    Node2 -> Node4 [xlabel = 'Edge  ']
+}")
+
+grViz("
+digraph {
+    node [style = filled fillcolor = none]
+    {
+        rank = same
+        BodyFat [fillcolor = 'LightBlue']
+        CVD [fillcolor = 'OrangeRed']
+    }
+    BodyFat -> CVD
+    Sex -> {BodyFat CVD Testosterone}
+    Testosterone -> {BodyFat CVD}
+}")
+
+grViz("
+digraph {
+    node [style = filled fillcolor = none]
+    {
+        rank = same
+        BodyFat [fillcolor = 'LightBlue']
+        CVD [fillcolor = 'OrangeRed']
+    }
+    BodyFat -> CVD
+    Sex -> {BodyFat CVD Testosterone ExerciseType}
+    Testosterone -> {BodyFat CVD}
+    ExerciseType -> {BodyFat CVD}
+}")
+
+grViz("
+digraph {
+    node [style = filled fillcolor = none]
+    {
+        rank = same
+        BodyFat [fillcolor = 'LightBlue']
+        CVD [fillcolor = 'OrangeRed']
+    }
+    BodyFat -> CVD
+    Sex -> {BodyFat CVD Testosterone ExerciseType}
+    Testosterone -> {BodyFat CVD}
+    ExerciseType -> {Testosterone BodyFat CVD}
+}")
