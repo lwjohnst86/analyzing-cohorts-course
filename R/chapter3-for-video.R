9fdbd3fc8c5c614ead45ef70d6c57666997ecb9d
Luke Johnston <lwjohnst@gmail.com>
2018-11-22 23:06:39 +0100

    remove extra code, start adding simpsons ex

--- R/chapter3-for-video.R
+++ R/chapter3-for-video.R
@@ -48,61 +48,32 @@ tribble(
     mutate(median_income = median_income * 10) %>%
     ggplot(aes(x = time, y = median_income)) +
     geom_smooth(method = "lm", se = FALSE, colour = "grey40") +
-    # geom_smooth(aes(group = job_type), method = "lm", se = FALSE, colour = "grey40") +
+    geom_smooth(aes(group = job_type), method = "lm", se = FALSE, colour = "grey40") +
     geom_point(colour = color_theme[6], size = 4)
 
+tribble(
+    ~Treatment, ~KidneyStoneSize, ~SuccessCases, ~TotalCases,
+    "A", "Small", 81, 87,
+    "A", "Large", 192, 263,
+    "B", "Small", 234, 270,
+    "B", "Large", 55, 80
+) %>%
+    group_by(Treatment) %>%
+    mutate(Total)
 
 
-
-# dag_example <-
-grViz("
-digraph {
+dag_example <- grViz(
+    "digraph {
+    graph [rankdir = LR]
     Node1 -> Node2 [dir = backward, xlabel = 'Edge ']
-    Node1 -> Node3 [dir = forward, xlabel = '     Edge']
-    Node2 -> Node4 [xlabel = 'Edge  ']
-}")
+    Node1 -> Node3 [dir = forward, label = '     Edge']
+    Node2 -> Node4 [label = 'Edge']
+    }"
+)
 
-grViz("
-digraph {
-    node [style = filled fillcolor = none]
-    {
-        rank = same
-        BodyFat [fillcolor = 'LightBlue']
-        CVD [fillcolor = 'OrangeRed']
-    }
-    BodyFat -> CVD
-    Sex -> {BodyFat CVD Testosterone}
-    Testosterone -> {BodyFat CVD}
-}")
-
-grViz("
-digraph {
-    node [style = filled fillcolor = none]
-    {
-        rank = same
-        BodyFat [fillcolor = 'LightBlue']
-        CVD [fillcolor = 'OrangeRed']
-    }
-    BodyFat -> CVD
-    Sex -> {BodyFat CVD Testosterone ExerciseType}
-    Testosterone -> {BodyFat CVD}
-    ExerciseType -> {BodyFat CVD}
-}")
-
-grViz("
-digraph {
-    node [style = filled fillcolor = none]
-    {
-        rank = same
-        BodyFat [fillcolor = 'LightBlue']
-        CVD [fillcolor = 'OrangeRed']
-    }
-    BodyFat -> CVD
-    Sex -> {BodyFat CVD Testosterone ExerciseType}
-    Testosterone -> {BodyFat CVD}
-    ExerciseType -> {Testosterone BodyFat CVD}
-}")
-
+grviz_to_png(dag_example,
+             here::here("datasets/ch3-v2-dag-example.png"),
+             width = 1500)
 
 # Video 2, show dagitty ---------------------------------------------------
 
